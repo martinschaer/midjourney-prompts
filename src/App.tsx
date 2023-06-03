fa4b5a95191771c074f3be8330b73c12f87266e6
Martin Schaer <martin@schaerweb.com>
2023-06-03 01:27:30 +0100

    add about and styles for menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,15 @@ import { Outlet, Link } from '@tanstack/router'
 function App() {
   return (
     <>
-      <div className="border-b">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+      <div className="border-b border-gray-700">
+        <div className="container mx-auto flex gap-4 p-2">
+          <Link to="/" activeProps={{ className: 'font-bold' }}>
+            Midjourney Prompts Generator
+          </Link>
+          <Link to="/about" activeProps={{ className: 'font-bold' }}>
+            About
+          </Link>
+        </div>
       </div>
       <Outlet />
     </>
--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,5 +1,19 @@
 function About() {
-  return <div className="container">About</div>
+  return (
+    <div className="card mx-auto my-10 max-w-prose">
+      <h1>Midjourney Prompt Generator</h1>
+      <h2>A systematic and growth-driven approach</h2>
+      <p className="my-4">
+        Save time and accelerate your process with this tool.
+      </p>
+      <p>
+        Created by{' '}
+        <a href="https://www.schaerweb.com" target="_self" rel="noreferrer">
+          Martin Schaer
+        </a>
+      </p>
+    </div>
+  )
 }
 
 export default About
