834cf44c1e088fada5981d6a9026167dd6221321
Martin Schaer <martin@schaerweb.com>
2023-06-06 23:06:30 +0100

    fix typo

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -2,7 +2,7 @@ function About() {
   return (
     <>
       <div className="card mx-auto my-10 max-w-prose">
-        <h1>Midjourney Prompt Generator</h1>
+        <h1>Midjourney Prompts Generator</h1>
         <h2>A systematic and growth-driven approach</h2>
         <p className="my-4">
           Save time and accelerate your process with this tool.
